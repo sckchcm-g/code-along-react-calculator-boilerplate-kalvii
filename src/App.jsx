fe17fbd23a63f502d8f26d9ce505c79d4ce37717
sg05c <141105618+sg05c@users.noreply.github.com>
2023-12-08 14:06:56 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,13 @@ const App = () => {
   // const handleDel = () => {
   //   setExpression(expression.slice(0, -1));
   // };
+
   const handleDel = () => {
-    setExpression(expression.slice(0, -1));
+    let newExp = expression;
+    if (typeof newExp !== 'string') {
+      newExp = expression.toString();
+    }
+    setExpression(newExp.slice(0, -1));
   };
 
   const handleAC = () => {
@@ -42,32 +47,32 @@ const App = () => {
           </div>
           <div>
             <div className='row first-row'>
-              <button onClick={handleAC}>AC</button>
-              <button onClick={handleDel}>Del</button>
-              <button onClick={() => handleAppend('/')}>/</button>
+              <button className='cnBtn' onClick={handleAC}>AC</button>
+              <button className='cnBtn' onClick={handleDel}>Del</button>
+              <button className='fnBtn' onClick={() => handleAppend('/')}>/</button>
             </div>
             <div className='row second-row'>
-              <button onClick={() => handleAppend(7)}>7</button>
-              <button onClick={() => handleAppend(8)}>8</button>
-              <button onClick={() => handleAppend(9)}>9</button>
-              <button onClick={() => handleAppend('*')}>*</button>
+              <button className='numBtn' onClick={() => handleAppend(7)}>7</button>
+              <button className='numBtn' onClick={() => handleAppend(8)}>8</button>
+              <button className='numBtn' onClick={() => handleAppend(9)}>9</button>
+              <button className='fnBtn' onClick={() => handleAppend('*')}>*</button>
             </div>
             <div className='row third-row'>
-              <button onClick={() => handleAppend(4)}>4</button>
-              <button onClick={() => handleAppend(5)}>5</button>
-              <button onClick={() => handleAppend(6)}>6</button>
-              <button onClick={() => handleAppend('-')}>-</button>
+              <button className='numBtn' onClick={() => handleAppend(4)}>4</button>
+              <button className='numBtn' onClick={() => handleAppend(5)}>5</button>
+              <button className='numBtn' onClick={() => handleAppend(6)}>6</button>
+              <button className='fnBtn' onClick={() => handleAppend('-')}>-</button>
             </div>
             <div className='row fourth-row'>
-              <button onClick={() => handleAppend(1)}>1</button>
-              <button onClick={() => handleAppend(2)}>2</button>
-              <button onClick={() => handleAppend(3)}>3</button>
-              <button onClick={() => handleAppend('+')}>+</button>
+              <button className='numBtn' onClick={() => handleAppend(1)}>1</button>
+              <button className='numBtn' onClick={() => handleAppend(2)}>2</button>
+              <button className='numBtn' onClick={() => handleAppend(3)}>3</button>
+              <button  className='fnBtn' onClick={() => handleAppend('+')}>+</button>
             </div>
             <div className='row fifth-row'>
-              <button onClick={() => handleAppend(0)}>0</button>
-              <button onClick={() => handleAppend('.')}>.</button>
-              <button onClick={handleEqual}>=</button>
+              <button className='numBtn' onClick={() => handleAppend(0)}>0</button>
+              <button className='numBtn' onClick={() => handleAppend('.')}>.</button>
+              <button  className='fnBtn' onClick={handleEqual}>=</button>
             </div>
           </div>
         </div>
